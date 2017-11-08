0e169ccb709fb79d1284acad17f72f1823f1eb58
bigdatadtu2017 <32615656+bigdatadtu2017@users.noreply.github.com>
2017-11-08 16:06:33 +0100

    Update challenge2ex1.py

--- challenge2ex1.py
+++ challenge2ex1.py
@@ -15,32 +15,25 @@ dataSQL = sqlite3.connect('reddit.db')
 dataSQL.text_factory = bytes
 c = dataSQL.cursor()
 
-n = 10
-topn_names = [None]*n
-topn_counts = [0]*n
-
-for row in c.execute('''SELECT subreddit_id, GROUP_CONCAT(body) 
-                         FROM
-                             (SELECT *
-                              FROM comments
-                              GROUP BY id
-                              ORDER BY id 
-                              )
-                         GROUP BY subreddit_id
-                         ORDER BY subreddit_id 
-                         '''):
-    name = str(row[0])
-    count = len(set(re.findall('\w+', str(row[1]).lower())))
-    if count > min(topn_counts):
-        topn_names[topn_counts.index(min(topn_counts))] = name
-        topn_counts[topn_counts.index(min(topn_counts))] = count
-
-list1, list2 = zip(*sorted(zip(topn_counts,topn_names), reverse = True))
-
-print(list1)
-print(list2)
+for row in c.execute('''WITH CTE AS
+                        (
+                        SELECT id, parent_id, subreddit_id, 0 as depth
+                        FROM comments
+                        UNION ALL
+                        SELECT c.id, c.parent_id, c.subreddit_id, CTE.depth + 1
+                        FROM comments AS c
+                        INNER JOIN CTE 
+                        ON c.parent_id = CTE.id
+                        )
+                        SELECT subreddit_id, avg(depth)
+                        FROM CTE
+                        GROUP BY subreddit_id
+                        ORDER BY avg(depth) DESC
+                        LIMIT 10
+                        '''):
+    print(row)
 print('done')
 end = time.time()
-print(end - start)
+print('run time [s]:',end - start)
 
 #%%
